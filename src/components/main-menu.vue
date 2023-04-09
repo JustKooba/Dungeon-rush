d3061a08acff2b6757c67cc9f9863f20dcc7d0b8
JustKooba <jakubsakw2009@gmail.com>
2023-04-09 19:43:01 +0200

    Finished main menu

--- src/components/main-menu.vue
+++ src/components/main-menu.vue
@@ -2,13 +2,25 @@
   <h1>Dungeon Rush</h1>
   <p>Welcome traveler.</p>
   <div class="content">
-    <div class="map">
-      <button class="map">
-        <img src="../assets/map_cleanup.png" alt="map icon" />
-      </button>
+    <div class="top">
+      <div class="map-wrapper">
+        <button class="map">
+          <img src="../assets/map_cleanup.png" alt="map icon" />
+        </button>
+      </div>
+      <div class="shop-wrapper">
+        <button class="shop">
+          <img src="../assets/shop.png" alt="shop icon" />
+        </button>
+      </div>
     </div>
-    <div class="shop"></div>
-    <div class="character"></div>
+    <div class="bottom">
+      <div class="character-wrapper">
+        <button class="character">
+          <img src="../assets/knight.png" alt="character icon" />
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,19 +33,49 @@ export default {
 <style scoped>
 .content {
   overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
-.map {
+
+.top {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 30px;
+}
+
+.map,
+.shop,
+.character {
   height: 400px;
 }
 
-.map:hover {
+.map {
+  margin-right: 50px;
+}
+
+.map:hover,
+.shop:hover,
+.character:hover {
   cursor: pointer;
   transform: rotate(1deg);
   transition: ease-in-out 0.3s;
 }
 
-.map img {
+.map img,
+.shop img,
+.character img {
   height: 100%;
   width: 300px;
+}
+
+h1,
+p {
+  text-align: center;
+}
+
+h1 {
+  margin-bottom: -20px;
 }
 </style>
