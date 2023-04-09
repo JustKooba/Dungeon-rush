112084de433d64d6023594d40970448561f023c6
JustKooba <jakubsakw2009@gmail.com>
2023-04-09 19:21:47 +0200

    Added map and main menu

--- src/components/main-menu.vue
+++ src/components/main-menu.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <h1>Dungeon Rush</h1>
+  <p>Welcome traveler.</p>
+  <div class="content">
+    <div class="map">
+      <button class="map">
+        <img src="../assets/map_cleanup.png" alt="map icon" />
+      </button>
+    </div>
+    <div class="shop"></div>
+    <div class="character"></div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +18,22 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.content {
+  overflow: hidden;
+}
+.map {
+  height: 400px;
+}
+
+.map:hover {
+  cursor: pointer;
+  transform: rotate(1deg);
+  transition: ease-in-out 0.3s;
+}
+
+.map img {
+  height: 100%;
+  width: 300px;
+}
+</style>
