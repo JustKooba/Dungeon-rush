4c007592ce9ae86204510e7154895f5bb4caa9e4
JustKooba <jakubsakw2009@gmail.com>
2023-04-09 18:54:03 +0200

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <main-menu />
+  </div>
 </template>
 
 <script>
+import mainMenu from "./components/main-menu.vue";
 export default {
   name: "App",
+  components: {
+    mainMenu,
+  },
 };
 </script>
 
-<style></style>
+<style>
+body {
+  background-color: #121212;
+  color: #fff;
+}
+</style>
---
+++ src/components/main-menu.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "mainMenu",
+};
+</script>
+
+<style></style>
