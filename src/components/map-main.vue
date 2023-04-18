a0dbc94d7083a9f3d69952f247d53e53f5b630a8
JustKooba <jakubsakw2009@gmail.com>
2023-04-18 16:12:50 +0200

    adding the dungeons

--- src/components/map-main.vue
+++ src/components/map-main.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
-    <h1>Map</h1>
-    <p>Map page</p>
-    <button @click="mapClick">Back</button>
+  <div class="content">
+    <h1>Choose your destination, traveler</h1>
+
+    <div class="dungeons">
+      <div class="Mine"></div>
+
+      <div class="Shadow-Crypts"></div>
+
+      <div class="Infernal-Stronghold"></div>
+
+      <div class="Cursed-Keep"></div>
+
+      <div class="Abyssal-Labyrinth"></div>
+    </div>
   </div>
 </template>
 
@@ -16,4 +26,40 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+@font-face {
+  font-family: diamond-dust;
+  src: url("../assets/font/DiamondDust-m3a9.ttf");
+}
+
+.content {
+  font-family: "diamond-dust", sans-serif;
+}
+
+h1 {
+  letter-spacing: 5px;
+  text-align: center;
+}
+
+.dungeons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.Mine {
+  background: url("../assets/mine.jpg");
+  height: 400px;
+  width: 300px;
+  background-repeat: no-repeat;
+}
+
+.Shadow-Crypts {
+  background: url("../assets/crypts.png");
+  height: 400px;
+  width: 300px;
+  background-repeat: no-repeat;
+}
+</style>
