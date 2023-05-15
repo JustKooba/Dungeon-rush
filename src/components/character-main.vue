d15ec5bacf4df8511902ee5efc26db749c48c458
JustKooba <jakubsakw2009@gmail.com>
2023-05-15 14:27:05 +0200

    added character inventory

--- src/components/character-main.vue
+++ src/components/character-main.vue
@@ -4,21 +4,41 @@
     <button @click="characterClick" class="back">
       <img src="../assets/back.svg" />
     </button>
-
-    <div class="inventory">
-      <div v-for="item in inventory" :key="item.index" class="item-box"></div>
+    <div class="bottom">
+      <div class="inv-wrap">
+        <h3>Inventory</h3>
+        <div class="inventory">
+          <div
+            v-for="item in inventory"
+            :key="item.index"
+            class="item-box"
+          ></div>
+        </div>
+      </div>
+      <div class="char-wrap">
+        <h3>Character Equipment</h3>
+        <div class="character">
+          <div
+            v-for="slot in character"
+            :key="slot.index"
+            class="item-box"
+          ></div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import inventoryData from "./data/inventory.json";
+import characterEqData from "./data/characterEq.json";
 
 export default {
   name: "characterMain",
   data() {
     return {
       inventory: inventoryData.inventory,
+      character: characterEqData.eq,
     };
   },
   methods: {
@@ -44,6 +64,11 @@ h1 {
   text-align: center;
 }
 
+h3 {
+  font-family: "Courier New", Courier, monospace;
+  text-align: center;
+}
+
 .back {
   background: transparent;
   position: absolute;
@@ -53,10 +78,32 @@ h1 {
   top: 10px;
 }
 
+.inventory {
+  display: grid;
+  grid-template-columns: repeat(5, 100px);
+  grid-template-rows: repeat(4, 100px);
+  justify-content: center;
+  margin-top: 15px;
+}
+
+.character {
+  display: grid;
+  grid-template-columns: repeat(2, 100px);
+  grid-gap: 12px;
+  justify-content: center;
+}
+
 .item-box {
   width: 80px;
   height: 80px;
   border: 1px solid rgb(204, 204, 204);
-  margin: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.bottom {
+  display: flex;
+  justify-content: space-around;
 }
 </style>
